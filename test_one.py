6bc4052dfd3a34c04ceb2679055186895f9dceae
noy72 <noy72@hatena.ne.jp>
2021-11-06 13:54:47 +0900

    スクリプト追加

--- test_one.py
+++ test_one.py
@@ -1,6 +1,7 @@
 from subprocess import Popen, PIPE, STDOUT
 from multiprocessing import Pool
 import pathlib
+import sys
 
 import pandas as pd
 
@@ -8,23 +9,28 @@ from tqdm import tqdm
 
 run_test = ['cargo', 'run', '--release', '--bin', 'tester', './a.out']
 
-def get_score(txt):
-    with open(txt, 'br') as f:
-        in_txt = f.read()
-
+def get_score(in_txt):
     p = Popen(run_test, stdout=PIPE, stdin=PIPE, stderr=PIPE, cwd='tools')
     result = p.communicate(input=in_txt)[1].decode()
     score = int(result.split('Score =')[1].strip())
 
     return score
 
+def iterator(in_txt):
+    for i in range(1000):
+        yield in_txt
+
 if __name__ == '__main__':
+    test_idx = sys.argv[1]
+    
     scores = []
-    input_file_pathes = [i for i in pathlib.Path('tools/in').glob('*.txt')]
+    with open(f'tools/in/{test_idx}.txt', 'br') as f:
+        in_txt = f.read()
+
     with Pool(4) as p:
         for s in tqdm(
-                p.imap_unordered(get_score, input_file_pathes),
-                total=len(input_file_pathes)
+                p.imap_unordered(get_score, iterator(in_txt)),
+                total=1000
                 ):
             scores.append(s)
 
